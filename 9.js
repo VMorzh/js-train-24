c84c6a283b8cbdc34ca2eb58b60efae1c66f64f4
VladimirNote <btk.distribution@gmail.com>
2023-07-26 19:13:40 +0300

    task 24 - is completed

--- 9.js
+++ 9.js
@@ -7,7 +7,18 @@ function task9() {
   // Викликаємо функцію fetchWithError
   // Якщо проміс виконаний успішно виводимо в консоль дані які він повертає
   // Якщо проміс виконаний з помилкою виводимо в консоль помилку
-}
+  function fetchWithError() {
+    const errorMessage = "Помилка при з'єднанні з сервером";
+    return Promise.reject(errorMessage);
+  }
 
+  fetchWithError()
+    .then((data) => {
+      console.log("Дані отримані успішно:", data);
+    })
+    .catch((error) => {
+      console.error("Помилка під час отримання даних:", error);
+    });
+}
 // Викликаємо функцію task10
 task9();
